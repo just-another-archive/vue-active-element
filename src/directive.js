9b01cc4fe441082ece05c1442249759ef393aed8
Julien Barbay <julien.barbay@linecorp.com>
2018-06-22 16:09:41 +0900

    adding focus events

--- src/directive.js
+++ src/directive.js
@@ -1,18 +1,22 @@
 export default {
   bind(root, { arg }, vnode) {
     const property = arg ? arg : 'active';
-    
+
     root._aehdlr_ = event => {
       if (property in vnode.context) {
         vnode.context[property] = event.target === root || root.contains(event.target);      
       }
     }
-    
-    document.addEventListener('click', root._aehdlr_)
+
+    root.addEventListener('focusin', root._aehdlr_);
+    root.addEventListener('focusout', root._aehdlr_);
+    document.addEventListener('click', root._aehdlr_);
   },
   
   unbind(root) {
     if (root._aehdlr_) {
+   	  root.removeEventListener('focusin', root._aehdlr_);
+      root.removeEventListener('focusout', root._aehdlr_);
       document.removeEventListener('click', root._aehdlr_);
       delete root._aehdlr_;
     }
