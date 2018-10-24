c3e5105c57d618e72ea7fac2fc27b1e5b0360bd9
Julien Barbay <barbay.julien@gmail.com>
2018-10-24 13:44:59 +0900

    update build system

--- rollup.build.js
+++ rollup.build.js
@@ -1,19 +1,36 @@
 const config = require('./package.json');
 
 const rollup = require('rollup'),
-      minify = require('rollup-plugin-minify-es')
+      minify = require('rollup-plugin-minify-es'),
+      babel  = require('rollup-plugin-babel')
 
-const i = { input: './src/index.js', plugins: [minify()] },
-      o = { file: config.main,
-            format: 'umd',
-            name: config.global
-          }
+const source = {
+  input: './src/index.js',
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    minify()
+  ]
+}
+
+const umd = {
+  file: config.main,
+  format: 'umd',
+  name: config.global,
+  exports: 'named',
+}
+
+const esm = {
+  file: config.module,
+  format: 'esm',
+}
 
 async function build() {
-  const bundle = await rollup.rollup(i)
-  const { code, map } = await bundle.generate(o)
+  const bundle = await rollup.rollup(source)
 
-  await bundle.write(o)
+  ;[umd, esm].forEach(build => {
+    bundle.generate(build)
+    bundle.write(build)
+  })
 }
 
 build()
