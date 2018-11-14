58318d31b37a36ca05ebceaa31225548f8b3bddd
Julien Barbay <barbay.julien@gmail.com>
2018-11-14 16:36:06 +0900

    better algorithm

--- src/directive.js
+++ src/directive.js
@@ -2,22 +2,31 @@ export default {
   bind(root, { arg }, vnode) {
     const property = arg ? arg : 'active';
 
-    root._aehdlr_ = event => {
-      if (property in vnode.context) {
-        vnode.context[property] = event.target === root || root.contains(event.target)
-      }
+    root._aehdlr_ = {
+      click : e => {
+        if (property in vnode.context)
+          vnode.context[property] = e.target === root || root.contains(e.target)
+      },
+      focusin : e => {
+        if (property in vnode.context)
+          vnode.context[property] = true
+      },
+      focusout: e => {
+        if (property in vnode.context && vnode.context[property])
+          vnode.context[property] = e.target === root || root.contains(e.target)
+      },
     }
 
-    root.addEventListener('focusin', root._aehdlr_);
-    root.addEventListener('focusout', root._aehdlr_);
-    document.addEventListener('click', root._aehdlr_);
+    root.addEventListener('focusin', root._aehdlr_.focusin);
+    root.addEventListener('focusout', root._aehdlr_.focusout);
+    document.addEventListener('click', root._aehdlr_.click);
   },
 
   unbind(root) {
     if (root._aehdlr_) {
-      root.removeEventListener('focusin', root._aehdlr_);
-      root.removeEventListener('focusout', root._aehdlr_);
-      document.removeEventListener('click', root._aehdlr_);
+      root.removeEventListener('focusin', root._aehdlr_.focusin);
+      root.removeEventListener('focusout', root._aehdlr_.focusout);
+      document.removeEventListener('click', root._aehdlr_.click);
       delete root._aehdlr_;
     }
   }
