b318db6fdcdfa50647923ae3022a6ef1040f7659
Julien Barbay <julien.barbay@linecorp.com>
2019-04-12 11:19:20 +0900

    fixing loop problem

--- src/directive.js
+++ src/directive.js
@@ -1,43 +1,53 @@
 export default {
-  bind(root, { expression }, vnode) {
-    const property = expression ? expression : 'active';
-
+  bind(root, { modifiers, expression = 'active' }, vnode) {
     root._aehdlr_ = {
       click : e => {
         const state = e.target === root || root.contains(e.target)
 
-        if (property in vnode.context)
-          return typeof vnode.context[property] === 'function'
-            ? vnode.context[property](state)
-            : vnode.context[property] = state
+        if (expression in vnode.context)
+          return vnode.context[expression] instanceof Function
+            ? vnode.context[expression](state)
+            : vnode.context[expression] = state
       },
-      focusin : e => {
-        if (property in vnode.context)
-          return typeof vnode.context[property] === 'function'
-            ? vnode.context[property](true)
-            : vnode.context[property] = true
-      },
-      focusout: e => {
-        const state = e.target === root || root.contains(e.target)
 
-        if (property in vnode.context)
-          return typeof vnode.context[property] === 'function'
-            ? vnode.context[property](state)
-            : vnode.context[property] = state
+      focusin : e => {
+        if (expression in vnode.context)
+          return vnode.context[expression] instanceof Function
+            ? vnode.context[expression](true)
+            : vnode.context[expression] = true
+      },
+
+      focusout: e => {
+        if (expression in vnode.context)
+          return vnode.context[expression] instanceof Function
+            ? vnode.context[expression](false)
+            : vnode.context[expression] = false
       },
     }
 
-    root.addEventListener('focusin', root._aehdlr_.focusin);
-    root.addEventListener('focusout', root._aehdlr_.focusout);
+    if (modifiers.partial !== true) {
+      root.addEventListener('focusin', root._aehdlr_.focusin);
+      root.addEventListener('focusout', root._aehdlr_.focusout);
+    }
+    else {
+      delete root._aehdlr_.focusin;
+      delete root._aehdlr_.focusout;
+    }
+
     document.addEventListener('click', root._aehdlr_.click);
   },
 
   unbind(root) {
-    if (root._aehdlr_) {
+    if (root._aehdlr_ && root._aehdlr_.focusin)
       root.removeEventListener('focusin', root._aehdlr_.focusin);
+
+    if (root._aehdlr_ && root._aehdlr_.focusout)
       root.removeEventListener('focusout', root._aehdlr_.focusout);
+
+    if (root._aehdlr_ && root._aehdlr_.click)
       document.removeEventListener('click', root._aehdlr_.click);
+
+    if (root._aehdlr_)
       delete root._aehdlr_;
-    }
   }
 }
