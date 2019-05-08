bfae0d747b241efdc936248d8282546759df49a9
Julien Barbay <julien.barbay@linecorp.com>
2019-05-08 13:50:29 +0900

    refactoring to allow interpreted expressions (value)

--- src/directive.js
+++ src/directive.js
@@ -1,53 +1,46 @@
 export default {
-  bind(root, { modifiers, expression = 'active' }, vnode) {
+  bind(root, { modifiers, expression = 'active', value }, { context }) {
+    const set = state => {
+      const property = value in context ? value : expression
+
+      if (property in context)
+        return context[property] instanceof Function
+          ? context[property](state)
+          : context[property] = state
+
+      return false
+    }
+
+    // _ Active Element HaNDLeR
     root._aehdlr_ = {
-      click : e => {
-        const state = e.target === root || root.contains(e.target)
-
-        if (expression in vnode.context)
-          return vnode.context[expression] instanceof Function
-            ? vnode.context[expression](state)
-            : vnode.context[expression] = state
-      },
-
-      focusin : e => {
-        if (expression in vnode.context)
-          return vnode.context[expression] instanceof Function
-            ? vnode.context[expression](true)
-            : vnode.context[expression] = true
-      },
-
-      focusout: e => {
-        if (expression in vnode.context)
-          return vnode.context[expression] instanceof Function
-            ? vnode.context[expression](false)
-            : vnode.context[expression] = false
-      },
+      click   : e => set(e.target === root || root.contains(e.target)),
+      focusin : () => set(true),
+      focusout: () => set(false),
     }
 
     if (modifiers.partial !== true) {
-      root.addEventListener('focusin', root._aehdlr_.focusin);
-      root.addEventListener('focusout', root._aehdlr_.focusout);
+      root.addEventListener('focusin', root._aehdlr_.focusin)
+      root.addEventListener('focusout', root._aehdlr_.focusout)
     }
     else {
-      delete root._aehdlr_.focusin;
-      delete root._aehdlr_.focusout;
+      delete root._aehdlr_.focusin
+      delete root._aehdlr_.focusout
     }
 
-    document.addEventListener('click', root._aehdlr_.click);
+    document.addEventListener('click', root._aehdlr_.click)
   },
 
   unbind(root) {
     if (root._aehdlr_ && root._aehdlr_.focusin)
-      root.removeEventListener('focusin', root._aehdlr_.focusin);
+      root.removeEventListener('focusin', root._aehdlr_.focusin)
 
     if (root._aehdlr_ && root._aehdlr_.focusout)
-      root.removeEventListener('focusout', root._aehdlr_.focusout);
+      root.removeEventListener('focusout', root._aehdlr_.focusout)
 
     if (root._aehdlr_ && root._aehdlr_.click)
-      document.removeEventListener('click', root._aehdlr_.click);
+      document.removeEventListener('click', root._aehdlr_.click)
 
     if (root._aehdlr_)
-      delete root._aehdlr_;
+      delete root._aehdlr_
   }
 }
